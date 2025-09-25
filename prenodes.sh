bcda10af9c28ac164f815fbf5311dc496bea8b5d
Shin Rag <zhdlcc@gmail.com>
2025-09-25 14:58:45 +0800

    update workflow

--- prenodes.sh
+++ prenodes.sh
@@ -3,5 +3,5 @@
 WORKDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd "$WORKDIR"
 # Path relative to this Pepo
-SERVER_HOME=$(find * -maxdepth 0 -type d | grep -E "^(ubuntu)$")
+SERVER_HOME=$(find * -maxdepth 0 -type d | grep -E "^(debian)$")
 find ${SERVER_HOME} -type d | sed 's|/$||g' | xargs -I{} touch "{}/index.md"
